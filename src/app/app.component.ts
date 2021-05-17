b88307e2df99c94e056f83f9681ba1b2a1ad70e5
Alvin Hsu <alvinhsu>
2021-05-17 17:10:19 +0800

    新增計算長度功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,43 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
   title = 'alvinTest4';
   items = [];
-  bb='';
-  bbC(){
+  bb = '';
+  danger = '';
+  bbC() {
+    this.danger = '';
+
     this.items = [];
     const index1 = this.bb.indexOf('app');
     const index2 = this.bb.indexOf('.');
     const index3 = this.bb.lastIndexOf('/');
     const index4 = this.bb.lastIndexOf('.ts');
     const index5 = this.bb.indexOf('.ts');
-    
-    this.items.push(this.bb.slice(index1,index3))
-    this.items.push(this.bb.slice(index3+1,index4))
-    this.items.push(this.bb.slice(index1,index5))
-    
+
+    this.items.push(this.bb.slice(index1, index3))
+    this.items.push(this.bb.slice(index3 + 1, index4))
+    this.items.push(this.bb.slice(index1, index5))
+
+    if ((this.items[2] + '.ts').length >= 160) {
+      this.danger = '請注意！長度已大於等於160'
+    }
+    if ((this.items[2] + '.ts').length >= 170) {
+      this.danger = '請注意！長度已大於等於170'
+    }
     // console.log(this.bb.slice(index1,index2));
     // console.log(this.bb.slice(index1,index3));
     // console.log(this.bb.slice(index3+1));
-    
-      }
+
+  }
+
+  copyEvent() {
+
+  }
 }
